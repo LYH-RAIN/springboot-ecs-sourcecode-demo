6b0e1d6aeec0fe34a42d79a57041930d80d88026
bupt_rain <lyh376240@alibaba-inc.com>
2024-09-13 16:09:39 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# 检查传入的参数
+if [ "$#" -ne 2 ]; then
+    echo "Usage: \$0 <username> <password>"
+    exit 1
+fi
+
+USERNAME=\$1
+PASSWORD=\$2
 
 sudo yum install -y java-1.8.0-openjdk
 echo y | sudo yum install java-1.8.0-openjdk-devel
@@ -8,4 +16,4 @@ echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
-sudo nohup java -jar demo-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar demo-0.0.1-SNAPSHOT.jar --username="$USERNAME" --password="$PASSWORD" > /dev/null 2>&1 &
